857c580ce319340421d411dd3199aa00c0c930bb
Your Name <you@example.com>
2024-09-12 14:18:48 +0300

    x

---
+++ Project1/Project1/FileName.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream>  // Підключаємо бібліотеку для вводу/виводу
+using namespace std;
+int main() {
+    cout << "Hello, dWorld!" << endl;  // Виводимо повідомлення
+    return 0;  // Повертаємо 0, що означає успішне завершення програми
+}
