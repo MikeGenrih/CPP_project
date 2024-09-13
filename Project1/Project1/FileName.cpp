4096b67fa77fa671fcc79d968dd7091370af1207
Your Name <you@example.com>
2024-09-13 21:10:03 +0300

    x

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -1,15 +1,38 @@
 ﻿#include <iostream>  
 using namespace std;
 
-int main() {
-    
-    string greeting = "Hello, ";
-    string name = "Mike";
-    string country = "Ukraine";
-    int year = 32;
+struct fruit
+{
+    string name;
+    int weight;
+    string data_to_good;
 
+};
+
+
+int main() {
+
+
+    fruit apple;
+    apple.name = "Apple";
+    apple.weight = 1;
+    apple.data_to_good = "5.08.2086";
+
+    fruit potato;
+    potato.name = "Potato";
+    potato.weight = 2;
+    potato.data_to_good = "5.08.2096";
+
+    cout << "Fruit name: " << apple.name << endl;
+    cout << "Weight: " << apple.weight << " kg" << endl;
+    cout << "Ripeness: " << apple.data_to_good <<"\n\n" << endl;
     
-    cout << greeting << "Awesome " << year << " year old " << name << " from " << country << endl;
+    cout << "Fruit name: " << potato.name << endl;
+    cout << "Weight: " << potato.weight << " kg" << endl;
+    cout << "Ripeness: " << potato.data_to_good << endl;
+
+
+    cout << "Fruit name: " << apple << endl;
 
     return 0; 
 }
