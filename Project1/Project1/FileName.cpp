e2fb575a2a842dfe17a196c6f5f45b2132b2bc22
Your Name <you@example.com>
2025-06-19 12:13:13 +0300

    fruit

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -1,39 +1,39 @@
-﻿#include <iostream>  
-using namespace std;
-
-struct fruit
-{
-    string name;
-    int weight;
-    string data_to_good;
-
-};
-
-
-int main() {
-
-
-    fruit apple;
-    apple.name = "Apple";
-    apple.weight = 1;
-    apple.data_to_good = "5.08.2086";
-
-    fruit potato;
-    potato.name = "Potato";
-    potato.weight = 2;
-    potato.data_to_good = "5.08.2096";
-
-    cout << "Fruit name: " << apple.name << endl;
-    cout << "Weight: " << apple.weight << " kg" << endl;
-    cout << "Ripeness: " << apple.data_to_good <<"\n\n" << endl;
-    
-    cout << "Fruit name: " << potato.name << endl;
-    cout << "Weight: " << potato.weight << " kg" << endl;
-    cout << "Ripeness: " << potato.data_to_good << endl;
-
-
-    cout << "Fruit name: " << apple << endl;
-
-    return 0; 
-}
-
+﻿//#include <iostream>  
+//using namespace std;
+//
+//struct fruit
+//{
+//    string name;
+//    int weight;
+//    string data_to_good;
+//
+//};
+//
+//
+//int main() {
+//
+//
+//    fruit apple;
+//    apple.name = "Apple";
+//    apple.weight = 1;
+//    apple.data_to_good = "5.08.2086";
+//
+//    fruit potato;
+//    potato.name = "Potato";
+//    potato.weight = 2;
+//    potato.data_to_good = "5.08.2096";
+//
+//    cout << "Fruit name: " << apple.name << endl;
+//    cout << "Weight: " << apple.weight << " kg" << endl;
+//    cout << "Ripeness: " << apple.data_to_good <<"\n\n" << endl;
+//    
+//    cout << "Fruit name: " << potato.name << endl;
+//    cout << "Weight: " << potato.weight << " kg" << endl;
+//    cout << "Ripeness: " << potato.data_to_good << endl;
+//
+//
+//    cout << "Fruit name: " << apple << endl;
+//
+//    return 0; 
+//}
+//
